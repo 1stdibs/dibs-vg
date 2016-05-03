1149c4b717d59e2bafcca694c5f2d791b86a0c3d
nameofname <nameofemail@gmail.com>
2016-05-03 19:41:00 -0400

    updated function name

--- generateList.js
+++ generateList.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 
-const build = () => {
+const buildList = () => {
     const ls = fs.readdirSync(__dirname + '/src');
     fs.writeFileSync(__dirname + '/list.json', JSON.stringify(ls), { encoding : "UTF-8" });
 };
 
-build();
+buildList();
