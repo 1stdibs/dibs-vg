d05186075364f302e1ca70c7c5ab0ea4b2d605ed
nameofname <nameofemail@gmail.com>
2016-05-03 19:51:21 -0400

    moar comments

--- js/SvgComponent.jsx
+++ js/SvgComponent.jsx
@@ -3,6 +3,22 @@
 const React = require('react');
 const { PropTypes } = React;
 
+/**
+ * SvgComponent provides an example of how you would use an icon with react. SVG files get required outside of this
+ * file and passed as children, this file provides a wrapper component.
+ * Example :
+ *      const Icon = require('dibs-vg/src/clock.svg');
+ *      const SvgComponent = require('dibs-vg/js/SvgComponent.svg');
+ *      // ... then inside your React component, you can do...
+ *              <SvgComponent size={25} className={myClass}>
+ *                  <Icon />
+ *              </SvgComponent>
+ *
+ *
+ * @param props
+ * @returns {XML}
+ * @constructor
+ */
 const SvgComponent = props => {
     let className = 'dibsvg-icon ' + props.className;
     className += props.size ? ' dibsvg-icon-' + props.size : '';
