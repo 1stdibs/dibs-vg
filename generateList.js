ceebe5f23d53580439edb398e40bbc384137664a
nameofname <nameofemail@gmail.com>
2016-05-23 18:26:18 -0400

    SVG fiels now get compiled to multi line template strings and react components

--- generateList.js
+++ generateList.js
@@ -1,14 +1,24 @@
 "use strict";
 
 const fs = require('fs');
+const checkError = err => {
+    if (err) {
+        throw err;
+        process.exit(1);
+    }
+};
 
 /**
  * Builds a JSON list of all the SVGs and writes to file. Important to include in the package in case someone needs a
  * list of all the icons ie. you want to render all of them.
  */
 const buildList = () => {
-    const ls = fs.readdirSync(__dirname + '/src');
-    fs.writeFileSync(__dirname + '/list.json', JSON.stringify(ls), { encoding : "UTF-8" });
+    fs.readdir(`${__dirname}/src`, (err, ls) => {
+        checkError(err);
+        fs.writeFile(`${__dirname}/list.json`, JSON.stringify(ls), { encoding : "UTF-8" }, err => {
+            checkError(err);
+        });
+    });
 };
 
 buildList();
