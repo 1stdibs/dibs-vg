61a4f4b68618d2f92200f22478a3e4d8bb7c6ec4
walker flynn <walker.d.flynn@gmail.com>
2016-07-06 16:24:30 -0400

    change the dist react components to be in plain js becasue that is more expected behavior from a library (#26)

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ React components are compiled and placed in dist/react, and for users who don't 
 Each pre-compiled React component supports a className prop so that you can specify styling. 
 
 ```js
-const svgComponent = require('dibs-vg/dist/react/account-filled.jsx');
+const svgComponent = require('dibs-vg/dist/react/account-filled.js');
 const stringSvg = require('dibs-vg/dist/templateString/account-filled.es.js');
 ```
 
--- generateDist.js
+++ generateDist.js
@@ -6,6 +6,7 @@
 
 const fs = require('fs');
 const htmlToReact = require('./htmlToReact');
+const babel = require('babel-core');
 const svgs = fs.readdirSync(process.cwd() + '/src');
 const stringDest = `${process.cwd()}/dist/templateString`;
 const reactDest = `${process.cwd()}/dist/react`;
@@ -16,29 +17,37 @@ const checkError = err => {
     }
 };
 
+let remaining = svgs.length;
+
+const promises = [];
+
 svgs.forEach(file => {
     const leFile = `${process.cwd()}/src/${file}`;
     const name = file.split('.')[0];
 
-    fs.readFile(leFile, (err, data) => {
-        checkError(err);
+    const contents = fs.readFileSync(leFile, {encoding: 'UTF-8'});
 
-        // generate template string module :
-        const fileContents = data.toString();
-        const stringContents = `module.exports = \`${fileContents}\`;`;
+    // generate template string module :
+    const stringContents = `module.exports = \`${contents}\`;`;
 
-        fs.writeFile(`${stringDest}/${name}.es.js`, stringContents, { encoding : 'UTF-8' }, err => {
-            checkError(err);
-        });
+    fs.writeFileSync(`${stringDest}/${name}.es.js`, stringContents, { encoding : 'UTF-8' });
 
-        // generate react module :
-        htmlToReact(fileContents).then(result => {
+    // generate react module :
+    promises.push( new Promise((resolve, reject) => {
+        htmlToReact(contents).then(result => {
+            console.log(`compiling react component for ${name}. ${remaining} files left`);
+            remaining --;
 
-            fs.writeFile(`${reactDest}/${name}.jsx`, result, { encoding : 'UTF-8' }, err => {
-                checkError(err);
-            });
-
-        }).catch(checkError);
-    });
+            const babelResult = babel.transform(result, {presets: ["es2015", "stage-2", "react"]});
+            fs.writeFileSync(`${reactDest}/${name}.js`, babelResult.code, { encoding : 'UTF-8' });
+            fs.writeFileSync(`${reactDest}/${name}.jsx`, result, { encoding : 'UTF-8' });
+            resolve();
+        }).catch(reject);
+    }));
 });
 
+Promise.all(promises).then(() => { 
+  process.exit(0);
+}, function(reason) {
+  checkError(reason);
+});
