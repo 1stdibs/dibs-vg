05ab977b3c71f83cdde74eea33d4cb0e0d0c5a34
dale tan <dale@1stdibs.com>
2016-07-16 21:18:14 -0400

    added icon search for react icons, css module classes

--- sample/js/sample.js
+++ sample/js/sample.js
@@ -4,12 +4,17 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const ReactIcons = require('./ReactIcons.jsx');
 const BackboneIcons = require('./BackboneIcons.js');
-require('../../scss/sample.scss');
 
 window.onload = function () {
     // React :
-    ReactDOM.render(<ReactIcons />, document.getElementsByClassName('sample-container')[0]);
-
+    ReactDOM.render(
+        React.createElement(ReactIcons, {}),
+        document.getElementsByClassName('sample-container')[0]
+    );
     // Backbone :
     document.getElementsByClassName('sample-container')[0].appendChild(new BackboneIcons().render().el);
 };
+
+if (module.hot) {
+    module.hot.accept();
+}
