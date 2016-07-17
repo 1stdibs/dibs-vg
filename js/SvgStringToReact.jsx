30ddd31250bc6d62addf52c3edcc8d8bdbebede3
dale tan <dale@1stdibs.com>
2016-07-17 12:20:48 -0400

    converted to stateless component, deleting unnecessary comment block

--- js/SvgStringToReact.jsx
+++ js/SvgStringToReact.jsx
@@ -1,29 +1,30 @@
+'use strict';
+
 const React = require('react');
 const { PropTypes } = React;
 
-const SvgStringToReact = React.createClass({
-    propTypes: {
-        svg: PropTypes.string,
-        styles: PropTypes.object,
-        width: PropTypes.number,
-        height: PropTypes.number
-    },
-    render() {
-        let styles = {
-            fill: "currentcolor",
-            verticalAlign: "middle"
-        };
-        return (
-            <svg 
-                viewBox="0 0 24 24"
-                width={this.props.width}
-                height={this.props.height || this.props.width}
-                preserveAspectRatio="xMidYMid meet"
-                fit
-                style= {Object.assign({}, styles, this.props.style)}
-                dangerouslySetInnerHTML={{__html: this.props.svg}} />
-        );
-    }
-});
+const SvgStringToReact = props => {
+    const styles = {
+        fill: "currentcolor",
+        verticalAlign: "middle"
+    };
+    return (
+        <svg
+            viewBox="0 0 24 24"
+            width={props.width}
+            height={props.height || props.width}
+            preserveAspectRatio="xMidYMid meet"
+            fit
+            style={Object.assign({}, styles, props.style)}
+            dangerouslySetInnerHTML={{__html: props.svg}} />
+    );
+};
+
+SvgStringToReact.propTypes = {
+    svg: PropTypes.string,
+    style: PropTypes.object,
+    width: PropTypes.number,
+    height: PropTypes.number
+};
 
 module.exports = SvgStringToReact;
