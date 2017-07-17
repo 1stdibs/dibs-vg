94137f5c1cc1929a9a0d8c4196443172a8340305
nameofname <nameofemail@gmail.com>
2017-07-17 13:08:03 -0400

    added CONTRIBUTING.MD derived html to sample file for gh-pages

--- sample/js/Wrapper.jsx
+++ sample/js/Wrapper.jsx
@@ -9,20 +9,50 @@ const React = require('react');
 const ReactIcons = require('./ReactIcons.jsx'); // eslint-disable-line react/require-extension
 const Logo = require('../../src/logo-no-reg.svg');
 const styles = require('../scss/sample.scss');
+const contributing = require('./contributing');
+function contributingMarkup() {
+    return {__html: contributing};
+}
 
-const Wrapper = () => {
-    return (
-        <div>
-            <div className={styles.nav}>
-                <a href="https://github.com/1stdibs/dibs-vg/blob/master/CONTRIBUTING.md">Guide To Contributing</a>
+
+class Wrapper extends React.Component {
+
+    constructor() {
+        super();
+        this.showContributing = this.showContributing.bind(this);
+    }
+
+    showContributing() {
+        this.contributing.style.display = 'block';
+    }
+
+    render () {
+        return (
+            <div>
+                <div className={styles.nav}>
+                    <a
+                        className={styles.button}
+                        onClick={this.showContributing}
+                        href="#Contributing"
+                    >
+                        View Guide To Contributing
+                    </a>
+                </div>
+                <div className={styles.logoWrapper}>
+                    <Logo className={styles.logo} />
+                    <span className={styles.logoCopy}>Icons</span>
+                </div>
+                <ReactIcons />
+                <div
+                    id="Contributing"
+                    style={{display: "none"}}
+                    ref={ele => { this.contributing = ele; }}
+                    dangerouslySetInnerHTML={contributingMarkup()}
+                >
+                </div>
             </div>
-            <div className={styles.logoWrapper}>
-                <Logo className={styles.logo} />
-                <span className={styles.logoCopy}>Icons</span>
-            </div>
-            <ReactIcons />
-        </div>
-    );
+        );
+    }
 };
 
 module.exports = Wrapper;
