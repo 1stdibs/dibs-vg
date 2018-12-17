1f61f4cbe528f31ef317fdc3cbee58e15593c932
Majd Murad <murad.majd@gmail.com>
2018-12-17 10:54:16 -0500

    move package

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    parserOptions: {
-        sourceType: 'module',
-    },
-    extends: ['eslint-config-1stdibs'],
-    rules: {
-        strict: 'off',
-    },
-};
--- scripts/deprecatedFileMap.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'trash-filled': 'trash-can',
-    'trash-outlined': 'trash-can'
-};
